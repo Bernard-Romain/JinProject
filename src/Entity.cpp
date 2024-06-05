1d12173c93f061c1d429b7defd9c6b5142f77294
pablo <pablo.brochard@gmail.com>
2024-06-05 15:46:32 +0200

    Class Entity

---
+++ src/Entity.cpp
@@ -0,0 +1,18 @@
+#include "Entity.h"
+
+void Entity::render()
+{
+}
+
+int Entity::isHit()
+{
+	return 0;
+}
+
+void Entity::collide()
+{
+}
+
+void Entity::update()
+{
+}
---
+++ src/Entity.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include "pugixml.hpp"
+#include <SFML/Graphics.hpp>
+
+class Entity
+{
+protected:
+	sf::Vector2f position;
+	sf::RectangleShape shape;
+	sf::Texture texture;
+public:
+	 void render();
+	 int isHit();
+	 void collide();
+	 void update();
+private:
+
+};
